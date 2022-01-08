69e6a33ad66aee0b126e7d3dd6955569fb6d7f4f
imtiyaz <mohammed.imtiyaz@decathlon.com>
2022-01-08 13:04:00 +0530

    adds the project

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Todo from "./Todo";
+import Header from "./Header";
+function App() {
+  return <><Header/><Todo /></>
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Header() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Imtiyaz you have to finish these tasks at any cost
+    </h1>
+  );
+}
+export default Header;
